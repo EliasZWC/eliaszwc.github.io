b33af133eccd0458260c0a3d2a54fdce2d634703
eliaszwc <eliaschang321@gmail.com>
2024-04-07 20:32:23 +0800

    1.0.1

--- pages/resource/resourceJS/list.js
+++ pages/resource/resourceJS/list.js
@@ -8,11 +8,11 @@ var resourceData = {
             "filename": "cauto.html"
         },
         {
-            "title": "Cauto",
-            "tag": ["Android"],
-            "img": "cauto.png",
-            "intro": "An AutoX-based automation software",
-            "filename": "cauto.html"
-        },
+            "title": "My Website",
+            "tag": ["Project"],
+            "img": "mywebsite.png",
+            "intro": "Elias Chang's personal website",
+            "filename": "mywebsite.html"
+        }
     ]
 }
